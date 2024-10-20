f193e692da3266feef407852e887de01d8a2d9c4
Elián Barrios <elianbarrios1707@outlook.com>
2024-10-20 12:43:27 -0600

    Agregados los modelos de Usuario y Dispositivo

---
+++ conecta2/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ESP8266.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Clave { get; set; } = null!;
+}
